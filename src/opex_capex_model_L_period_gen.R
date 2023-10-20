27384b68b28ffa68bc2c3214525124b7fab21b4d
jwingenroth <jwing421@gmail.com>
2023-10-20 11:56:29 -0400

    Switch to L-period model

--- src/opex_capex_model_L_period_gen.R
+++ src/opex_capex_model_L_period_gen.R
@@ -18,29 +18,41 @@ r = 0.1
 # Capital
 k.g.0 = 450
 mu.g = 0
+# mu.g = -0.10
 sigma.g = 0.05
+# sigma.g = 0.05*10
 # Operating
 c.g = 0
 
 ## Fossil cost parameters
 # Capital
-k.f = 277.5 # for L=1
-# k.f = 278 # for L=10. To do: update
+# k.f = 277.5
+k.f = 278 # for L=10
 
 # Operating
-c.f.0 = 19 # for L=1
+# c.f.0 = 19 # for L=1
+c.f.0 = 28 # for L=10
 mu.f = 0
-# sigma.f = 0.1118 # for L=10. To do: update
-sigma.f = 0.09133 # for L=1
+# mu.f = 0.10
+sigma.f = 0.1118 # for L=10. To do: update
+# sigma.f = 0.09133 # for L=1
+# sigma.f = 0.09133/1000 # for L=1
 
 # Asset life
-# L = 10 # To do: update
-L = 1
+L = 10 # To do: update
+# L = 1
 DF = 1/(1+r)^(1:L)
 
 # Output/year
-# q = 1 # for L=10 (To do)
-q = 10
+q = 1 # for L=10 (To do)
+# q = 10
+
+# Try this: what if the second asset has the same cost profile as fossil, but
+# just was independent?
+# k.g.0 = k.f
+# c.g = c.f.0
+# sigma.g = sigma.f
+
 
 # Simulate cost process
 # GBM function
@@ -98,7 +110,7 @@ dgbm = function(x, mu, sigma, t, x0) {
 t=10
 # Check:
 dgbm(k.g.grid, mu=mu.g, sigma=sigma.g, t=t, x0=k.g.0)/
-dlnorm(k.g.grid, meanlog=log(k.g.0) + (mu.g-1/2*sigma.g^2)*t, sdlog=sigma.g*sqrt(t))
+  dlnorm(k.g.grid, meanlog=log(k.g.0) + (mu.g-1/2*sigma.g^2)*t, sdlog=sigma.g*sqrt(t))
 
 # Compute diffusion matrices
 # (To do: change to also calculate these for 1 through L?)
@@ -106,19 +118,19 @@ Phi.g <- Phi.g.10 <- matrix(0, nrow=grid.length.g, ncol=grid.length.g)
 rownames(Phi.g) <- colnames(Phi.g) <- k.g.grid
 rownames(Phi.g.10) <- colnames(Phi.g.10) <- k.g.grid
 
-Phi.f <- Phi.f.10 <- matrix(0, nrow=grid.length.f, ncol=grid.length.f)
+Phi.f <- Phi.f.L <- matrix(0, nrow=grid.length.f, ncol=grid.length.f)
 rownames(Phi.f) <- colnames(Phi.f) <- c.f.grid
-rownames(Phi.f.10) <- colnames(Phi.f.10) <- c.f.grid
+rownames(Phi.f.L) <- colnames(Phi.f.L) <- c.f.grid
 
 # for c.f
 for (i in 1:grid.length.f) {
   Phi.f[i,] =    dgbm(c.f.grid, mu=mu.f, sigma=sigma.f, x0=c.f.grid[i], t=1)/sum(dgbm(c.f.grid, mu=mu.f, sigma=sigma.f, x0=c.f.grid[i], t=1))
-  Phi.f.10[i,] = dgbm(c.f.grid, mu=mu.f, sigma=sigma.f, x0=c.f.grid[i], t=10)/sum(dgbm(c.f.grid, mu=mu.f, sigma=sigma.f, x0=c.f.grid[i], t=10))
+  Phi.f.L[i,] = dgbm(c.f.grid, mu=mu.f, sigma=sigma.f, x0=c.f.grid[i], t=10)/sum(dgbm(c.f.grid, mu=mu.f, sigma=sigma.f, x0=c.f.grid[i], t=L))
 }
 # for k.g
 for (i in 1:grid.length.g) {
   Phi.g[i,] =    dgbm(k.g.grid, mu=mu.g, sigma=sigma.g, x0=k.g.grid[i], t=1)/sum(dgbm(k.g.grid, mu=mu.g, sigma=sigma.g, x0=k.g.grid[i], t=1))
-  Phi.g.10[i,] = dgbm(k.g.grid, mu=mu.g, sigma=sigma.g, x0=k.g.grid[i], t=10)/sum(dgbm(k.g.grid, mu=mu.g, sigma=sigma.g, x0=k.g.grid[i], t=10))
+  Phi.g.10[i,] = dgbm(k.g.grid, mu=mu.g, sigma=sigma.g, x0=k.g.grid[i], t=10)/sum(dgbm(k.g.grid, mu=mu.g, sigma=sigma.g, x0=k.g.grid[i], t=L))
 }
 range(apply(Phi.g, 1, sum))
 range(apply(Phi.g.10, 1, sum))
@@ -142,8 +154,8 @@ for (i in 1:grid.length.f) for (j in 1:grid.length.g) {
   Phi.f.g[[i,j]]  = Phi.f.g[[i,j]] / sum(Phi.f.g[[i,j]] ) # normalize to a probability matrix
   
   # for t=10 steps ahead (To do: change to calculate these for 1 through L?)
-  # Phi.f.g.10[[i,j]] = rep.row(Phi.f.10[i,], grid.length.g) * rep.col(Phi.g.10[j,], grid.length.f) # old
-  Phi.f.g.10[[i,j]] = rep.row(Phi.g.10[j,], grid.length.f) * rep.col(Phi.f.10[i,], grid.length.g) 
+  # Phi.f.g.10[[i,j]] = rep.row(Phi.f.L[i,], grid.length.g) * rep.col(Phi.g.10[j,], grid.length.f) # old
+  Phi.f.g.10[[i,j]] = rep.row(Phi.g.10[j,], grid.length.f) * rep.col(Phi.f.L[i,], grid.length.g) 
   Phi.f.g.10[[i,j]]  = Phi.f.g.10[[i,j]] / sum(Phi.f.g.10[[i,j]] ) # normalize to a probability matrix
   
   rownames(Phi.f.g[[i,j]]) <- rownames(Phi.f.g.10[[i,j]]) <-  c.f.grid
@@ -200,8 +212,8 @@ V.calc = function(V) {
   # For each potential c.f and k.g value, calculate npv costs and the optimal investment
   for (i in 1:grid.length.f) for (j in 1:grid.length.g) {
     # To do: change C.f to include expected path of c.f through h=1,..,L, rather than just 1 year
-    C.f = k.f + c.f.grid[i]*exp(mu.f*1)*q*DF + sum(Phi.f.g[[i, j]]*V)/(1+r)^L
-    C.g = k.g.grid[j] + c.g*q*DF + sum(Phi.f.g[[i, j]]*V)/(1+r)^L
+    C.f = k.f + sum(c.f.grid[i]*exp(mu.f*(1:L))*q*DF) + sum(Phi.f.g.10[[i, j]]*V)/(1+r)^L
+    C.g = k.g.grid[j] + sum(c.g*q*DF) + sum(Phi.f.g.10[[i, j]]*V)/(1+r)^L
     
     C.vec = c(C.f, C.g)
     opt.strat[i,j] = which.min(C.vec)
@@ -230,36 +242,146 @@ iter
 V.out = V.calc(V)
 
 # Examine solved model
-par(mfrow=c(2,2))
-image(z=V.out$Vnew, x=c.f.grid, y=k.g.grid, main='NPV of costs (optimal)')
+par(mfrow=c(1,3))
+image(z=-V.out$Vnew, x=c.f.grid, y=k.g.grid, main='NPV of costs (optimal)', col=hcl.colors(200, palette='Heat'), xlab='Fossil Operating Cost', ylab='Green Capital Cost')
 points(x=c.f.0, y=k.g.0, pch=19, cex=3, col=4)
 
 image(V.out$npvcost['green',,]-V.out$npvcost['fossil',,], x=c.f.grid, y=k.g.grid,
-      main='Green advantage (difference in $ NPV cost)',
-      col=hcl.colors(12, palette='Green-Orange'))
+      main='Green advantage\n(difference in $ NPV cost)',
+      xlab='Fossil Operating Cost', ylab='Green Capital Cost',
+      col=rev(hcl.colors(200, palette='Red-Green')))
 
-image(V.out$opt.strat, x=c.f.grid, y=k.g.grid, main='Optimal strategy (darker area means green)')
+image(-V.out$opt.strat, x=c.f.grid, y=k.g.grid, main='Optimal current-period strategy', col=hcl.colors(200, palette='Green-Orange'), xlab='Fossil Operating Cost', ylab='Green Capital Cost')
 points(x=c.f.0, y=k.g.0, pch=19, cex=3, col=4)
-# because our c.f.0 and k.g.0 values equated the expectation and variance of costs
-# between the two assets, we are on an edge case where very small changes
-# in the level of either cost parameter can flip the decision at these prices
 
+# because our c.f.0 and k.g.0 values equated the expectation and variance of costs
+# between the two assets, we are on an edge case where very small changes in either
+# in the level of either cost parameter can flip the decision at these prices
+mean(V)
+mean(V.out$npvcost['green',,]-V.out$npvcost['fossil',,])
 # Optimal strategy 
 V.out$opt.strat[as.character(c.f.0), as.character(k.g.0)]
 V.out$opt.strat[as.character((c.f.0-2):(c.f.0+2)), as.character(seq(k.g.0-20,k.g.0+20, by=10))]
 
-# NPV Benefit of having green = ENPV cost of perpetual fossil investment vs actual NPV cost
-green.benefit = rep.col((k.f + c.f.grid*exp(mu.f*1)*q*DF)/r, grid.length.g)  - V.out$Vnew # question: does only hold if mu.f=0? I think yes
-image(green.benefit, x=c.f.grid, y=k.g.grid)
-hist(green.benefit)
-mean(green.benefit)
+V.out$Vnew[as.character(c.f.0), as.character(k.g.0)]
+V.out$Vnew[as.character((c.f.0-2):(c.f.0+2)), as.character(seq(k.g.0-20,k.g.0+20, by=10))]
 
-# NPV Benefit of having fossil = ENPV cost of perpetual green investment vs actual NPV cost
-fossil.benefit = (k.g.0 + c.g*q*DF)/r  - V.out$Vnew  # question: does only hold if mu.g=0? I think yes
-image(fossil.benefit, x=c.f.grid, y=k.g.grid)
-hist(fossil.benefit)
-mean(fossil.benefit)
+### Run with only green, and again with only fossil
+# Do the optimization with green as the only option. 
+# The easiest but brute-forcey way to do this is to simply replace C.f with C.g's value, so the options are equivalent.
+V.calc.onlyg = function(V) {
+  Vnew = array(runif(grid.size), dim=c(grid.length.f, grid.length.g),
+               dimnames=list(c.f.grid, k.g.grid))
+  
+  C.array = array(NA, dim=c(2, length(c.f.grid), length(k.g.grid)), 
+                  dimnames = list(c('fossil','green'),
+                                  c.f.grid, k.g.grid))
+  
+  # For each potential c.f and k.g value, calculate npv costs and the optimal investment
+  for (i in 1:grid.length.f) for (j in 1:grid.length.g) {
+    # C.f = k.f + sum(c.f.grid[i]*exp(mu.f*(1:L))*q*DF) + sum(Phi.f.g.10[[i, j]]*V)/(1+r)^L
+    C.g = k.g.grid[j] + sum(c.g*q*DF) + sum(Phi.f.g.10[[i, j]]*V)/(1+r)^L
+    C.f = C.g
+    
+    C.vec = c(C.f, C.g)
+    opt.strat[i,j] = which.min(C.vec)
+    C.array[,i,j] = C.vec
+    
+    Vnew[i,j] = min(C.vec)
+  }
+  
+  return(list(Vnew=Vnew, opt.strat=opt.strat, npvcost=C.array))
+}
 
-# not sure why some of these are negative--having the choice should always be strictly better
-# than not having it, since we always take the min of C.f and C.g. This must be wrong.
-# I think we can figure this out by just eliminating the green (or fossil) asset from the model and solving.
+iter = 0
+delta = 1
+st = Sys.time()
+while (delta>1e-6) {
+  Vnew.onlyg = V.calc.onlyg(V)$Vnew
+  delta = max(Vnew.onlyg-V)
+  V = Vnew.onlyg
+  iter = iter+1
+}
+ed = Sys.time()
+difftime(ed, st) 
+
+V.g = V.calc.onlyg(V)
+
+par(mfrow=c(2,2))
+image(z=V.g$Vnew, x=c.f.grid, y=k.g.grid, main='NPV of costs (optimal)',xlab='Fossil Operating Cost', ylab='Green Capital Cost')
+points(x=c.f.0, y=k.g.0, pch=19, cex=3, col=4)
+
+image(V.g$npvcost['green',,]-V.g$npvcost['fossil',,], x=c.f.grid, y=k.g.grid,
+      main='Green advantage (difference in $ NPV cost)',
+      xlab='Fossil Operating Cost', ylab='Green Capital Cost',
+      col=rev(hcl.colors(200, palette='Red-Green')))
+
+image(V.g$opt.strat, x=c.f.grid, y=k.g.grid, main='Optimal strategy', col=hcl.colors(200, palette='Green-Orange'), xlab='Fossil Operating Cost', ylab='Green Capital Cost')
+points(x=c.f.0, y=k.g.0, pch=19, cex=3, col=4)
+
+# Do the optimization with fossil as the only option. 
+# The easiest but brute-forcey way to do this is to simply replace C.g with C.f's value, so the options are equivalent.
+V.calc.onlyf = function(V) {
+  Vnew = array(runif(grid.size), dim=c(grid.length.f, grid.length.g),
+               dimnames=list(c.f.grid, k.g.grid))
+  
+  C.array = array(NA, dim=c(2, length(c.f.grid), length(k.g.grid)), 
+                  dimnames = list(c('fossil','green'),
+                                  c.f.grid, k.g.grid))
+  
+  # For each potential c.f and k.g value, calculate npv costs and the optimal investment
+  for (i in 1:grid.length.f) for (j in 1:grid.length.g) {
+    C.f = k.f + sum(c.f.grid[i]*exp(mu.f*(1:L))*q*DF) + sum(Phi.f.g.10[[i, j]]*V)/(1+r)^L
+    # C.g = k.g.grid[j] + sum(c.g*q*DF) + sum(Phi.f.g.10[[i, j]]*V)/(1+r)^L
+    C.g = C.f
+    
+    C.vec = c(C.f, C.g)
+    opt.strat[i,j] = which.min(C.vec)
+    C.array[,i,j] = C.vec
+    
+    Vnew[i,j] = min(C.vec)
+  }
+  
+  return(list(Vnew=Vnew, opt.strat=opt.strat, npvcost=C.array))
+}
+
+iter = 0
+delta = 1
+st = Sys.time()
+while (delta>1e-6) {
+  Vnew.onlyg = V.calc.onlyf(V)$Vnew
+  delta = max(Vnew.onlyg-V)
+  V = Vnew.onlyg
+  iter = iter+1
+}
+ed = Sys.time()
+difftime(ed, st) 
+
+V.f = V.calc.onlyf(V)
+
+par(mfrow=c(2,2))
+image(z=V.f$Vnew, x=c.f.grid, y=k.g.grid, main='NPV of costs (optimal)',xlab='Fossil Operating Cost', ylab='Green Capital Cost')
+points(x=c.f.0, y=k.g.0, pch=19, cex=3, col=4)
+
+image(V.f$npvcost['green',,]-V.f$npvcost['fossil',,], x=c.f.grid, y=k.g.grid,
+      main='Green advantage (difference in $ NPV cost)',
+      xlab='Fossil Operating Cost', ylab='Green Capital Cost',
+      col=rev(hcl.colors(200, palette='Red-Green')))
+
+image(V.f$opt.strat, x=c.f.grid, y=k.g.grid,  main='Optimal strategy', col=hcl.colors(200, palette='Green-Orange'), xlab='Fossil Operating Cost', ylab='Green Capital Cost')
+points(x=c.f.0, y=k.g.0, pch=19, cex=3, col=4)
+
+green.value = V.out$Vnew - V.f$Vnew
+fossil.value = V.out$Vnew - V.g$Vnew
+delta.value = (green.value - fossil.value) # negative means green is better
+
+png(filename = paste0('output/figures/option_value_L',L,'_',Sys.Date(),'.png'), width = 480*3, height=480, pointsize = 24)
+# png(filename = paste0('output/figures/option_value_L',L,'_drift_',Sys.Date(),'.png'), width = 480*3, height=480, pointsize = 24)
+par(mfrow=c(1,3))
+image(z=fossil.value, x=c.f.grid, y=k.g.grid, main='Fossil option value', col=hcl.colors(200, palette='BluGrn'), xlab='Fossil Operating Cost', ylab='Green Capital Cost')
+image(z=green.value, x=c.f.grid, y=k.g.grid, main='Green option value', col=hcl.colors(200, palette='BluGrn'), xlab='Fossil Operating Cost', ylab='Green Capital Cost')
+image(z=(green.value - fossil.value), x=c.f.grid, y=k.g.grid, main='Green option value over fossil', col=hcl.colors(200, palette='Green-Orange'), xlab='Fossil Operating Cost', ylab='Green Capital Cost')
+dev.off()
+
+green.value[as.character((c.f.0-2):(c.f.0+2)), as.character(seq(k.g.0-20,k.g.0+20, by=10))]
+fossil.value[as.character((c.f.0-2):(c.f.0+2)), as.character(seq(k.g.0-20,k.g.0+20, by=10))]
